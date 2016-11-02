2851966d9b6bbbe84c6b09fb21c7df90c52fc517
sumi-kumar <sumitshandilya007@gmail.com>
2016-11-02 11:22:38 +0530

    Add paerclip image in article model to upload image

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,7 @@
 class Article < ActiveRecord::Base
   belongs_to :owner
+  has_attached_file :avatar, styles: { medium: "150x150>", thumb: "60x60>" }, default_url: "/images/:style/missing.png"
+  validates_attachment :avatar, presence: true,
+  content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] },
+  size: { in: 0..5.megabytes }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161027124655) do
+ActiveRecord::Schema.define(version: 20161102053544) do
 
   create_table "albums", force: :cascade do |t|
     t.string   "name"
@@ -25,8 +25,12 @@ ActiveRecord::Schema.define(version: 20161027124655) do
     t.text     "description"
     t.float    "price"
     t.integer  "owner_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "comments", force: :cascade do |t|
